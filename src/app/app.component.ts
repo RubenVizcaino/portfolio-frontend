5a29fab6eb77bada5ed995823897a8937b7df701
DESKTOP-A129F57\Ruben Programador <rubeen1wp@gmail.com>
2025-06-03 12:09:00 +0200

    Create components for projects and hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeroComponent } from "./hero/hero.component";
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // 👈 Si esto está presente
-  imports: [CommonModule], // 👈 Asegúrate de incluir CommonModule aquí
+  imports: [CommonModule, HeroComponent,ProjectsComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public title = 'frontend';
-  proyectos = [
-    {
-      titulo: 'Landing Page',
-      descripcion: 'Diseño responsivo de una landing page moderna.',
-      link: 'https://ejemplo.com/landing'
-    },
-    {
-      titulo: 'App de Clima',
-      descripcion: 'Consulta el clima actual usando una API.',
-      link: 'https://ejemplo.com/clima'
-    },
-    {
-      titulo: 'Portafolio UX/UI',
-      descripcion: 'Diseño y estructura de un portafolio visual.',
-      link: 'https://ejemplo.com/uxui'
-    }
-  ];
 }
